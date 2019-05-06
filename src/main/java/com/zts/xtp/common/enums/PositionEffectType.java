2b12f040d9ac713f69cc12e67bab983461ad9c72
郑魏义 <zhengwy@zts.com.cn>
2019-05-06 11:24:32 +0800

    增加 开平标识类型

--- src/main/java/com/zts/xtp/common/enums/PositionEffectType.java
+++ src/main/java/com/zts/xtp/common/enums/PositionEffectType.java
@@ -21,6 +21,14 @@ public enum PositionEffectType {
     XTP_POSITION_EFFECT_FORCEOFF,
     /**本地强平*/
     XTP_POSITION_EFFECT_LOCALFORCECLOSE,
+    /**信用业务追保强平*/
+    XTP_POSITION_EFFECT_CREDIT_FORCE_COVER,
+    /**信用业务清偿强平*/
+    XTP_POSITION_EFFECT_CREDIT_FORCE_CLEAR,
+    /**信用业务合约到期强平*/
+    XTP_POSITION_EFFECT_CREDIT_FORCE_DEBT,
+    /**信用业务无条件强平*/
+    XTP_POSITION_EFFECT_CREDIT_FORCE_UNCOND,
     /**未知的开平标识类型*/
     XTP_POSITION_EFFECT_UNKNOWN
 }
